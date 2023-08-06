9be9a49360cd76827b8bf23b449331b74616b9de
DESKTOP-EH3E6F0\shixiansheng <1323119507@qq.com>
2023-08-06 13:06:56 +0800

    marst   commit

---
+++ src/main/java/com/example/priject/Controler/MasertContrller.java
@@ -0,0 +1,9 @@
+package com.example.priject.Controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/master")
+public class MasertContrller {
+}
